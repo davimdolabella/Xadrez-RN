4d04d9e7e1b34001184d742e588e406b92e374a9
davimdolabella <davimdolabella03@gmail.com>
2025-04-05 18:30:07 -0300

    Corrigido: removido submódulo da pasta xadrez

---
+++ xadrez/src/Xadrez.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+export default class Xadrez extends React.Component{
+    reander(){
+        return(
+            <Text>Olá Xadrez</Text>
+        )
+    }
+}
