ccefce0c54d0d0d4a7853e4021f64b5e3a7b2d4c
davimdolabella <davimdolabella03@gmail.com>
2025-04-09 19:30:09 -0300

    adicionando lógica e corrigindo erros

--- xadrez/src/Xadrez.js
+++ xadrez/src/Xadrez.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Dimensions, Text, TouchableHighlight } from 'react-native'
+import { View, Dimensions, Text, TouchableOpacity } from 'react-native'
 import Quadrado from './components/Quadrado'
 import Peca from './components/Peca'
 
-const lins = 'abcdefgh'
-
+const cols = 'abcdefgh'
+const pecas = ['p', 't', 'c', 'b', 'ra', 're']
 const initialTabuleiro = [
   ['t_b', 'c_b', 'b_b', 'ra_b', 're_b', 'b_b', 'c_b', 't_b'],
   ['p_b', 'p_b', 'p_b', 'p_b', 'p_b', 'p_b', 'p_b', 'p_b'],
@@ -33,14 +33,26 @@ export default class Xadrez extends React.Component {
     selects: [null, null],
     select: false,
   }
-
+  getpecadetail(lin, col){
+    let tabuleiro = this.state.tabuleiro
+    let peca_detail = tabuleiro[lin][col] !== null ? tabuleiro[lin][col].split('_') : [null, null]
+    return peca_detail
+  }
   pecaclicada = (peca_detail) => {
-    if (this.state.selects[0] !== null && (this.state.selects[0].cor === peca_detail.cor || (peca_detail.nome === null && !peca_detail.selectable)) ) {
+    if (this.state.selects[0] !== null && (peca_detail.nome === null && !peca_detail.selectable))  {
       this.setState({
         selects: [null, null],
         select: false,
       })
       this.limparcasaspossiveis()
+      
+    }else if(this.state.selects[0] !== null && peca_detail.cor === this.state.turn){
+      this.limparcasaspossiveis()
+      this.setState({
+        selects: [peca_detail, null],
+        select: true,
+      })
+      this.mostrarcasaspossiveis(peca_detail)
     }
     if (this.state.select === false && peca_detail.nome !== null && peca_detail.cor === this.state.turn) {
       this.setState({
@@ -52,8 +64,8 @@ export default class Xadrez extends React.Component {
     }else if(peca_detail.selectable){
         let tabuleiro = this.state.tabuleiro
         let peca = this.state.selects[0]
-        tabuleiro[peca.col][peca.lin] = null
-        tabuleiro[peca_detail.col][peca_detail.lin] = peca.nome+'_'+ peca.cor + '_n_'+ peca.piecemoves + 1
+        tabuleiro[peca.lin][peca.col] = null
+        tabuleiro[peca_detail.lin][peca_detail.col] = peca.nome+'_'+ peca.cor + '_n_'+ peca.piecemoves + 1
             this.setState({
                 selects: [null, null],
                 select: false,
@@ -65,14 +77,11 @@ export default class Xadrez extends React.Component {
   }
   limparcasaspossiveis = () => {
     let tabuleiro = this.state.tabuleiro
-    Array.from({ length: 8 }).map((_, lin) => (
-      Array.from({ length: 8 }).map((_, col) => {
-        let peca_detail =
-          this.state.tabuleiro[col][lin] !== null
-            ? this.state.tabuleiro[col][lin].split('_')
-            : [null, null]
+    Array.from({ length: 8 }).map((_, col) => (
+      Array.from({ length: 8 }).map((_, lin) => {
+        let peca_detail = this.getpecadetail(lin,col)
         if (peca_detail[2] === 'o') {
-          tabuleiro[col][lin] = peca_detail[0] + '_' + peca_detail[1] + '_n_' + peca_detail[3]
+          tabuleiro[lin][col] = peca_detail[0] + '_' + peca_detail[1] + '_n_' + peca_detail[3]
         }
       })
     ))
@@ -85,26 +94,53 @@ export default class Xadrez extends React.Component {
     let tabuleiro =  this.state.tabuleiro
     if (peca_detail.nome === 'p'){
       if (peca_detail.cor === 'w') {
-        posicpossiveis.push([peca_detail.col- 1, peca_detail.lin ])
-        if (peca_detail.piecemoves === 0) {
-          posicpossiveis.push([peca_detail.col - 2, peca_detail.lin])
+        let diagonaldireita = this.getpecadetail(peca_detail.lin -1, peca_detail.col + 1)
+        let diagonalesquerda = this.getpecadetail(peca_detail.lin -1, peca_detail.col - 1)
+        if (pecas.includes(diagonaldireita[0]) && diagonaldireita[1] !== peca_detail.cor){
+          posicpossiveis.push([peca_detail.lin -1 , peca_detail.col + 1])
+        }
+        if (pecas.includes(diagonalesquerda[0]) && diagonalesquerda[1] !== peca_detail.cor){
+          posicpossiveis.push([peca_detail.lin -1 , peca_detail.col - 1])
+        }
+        let andada1 = this.getpecadetail(peca_detail.lin - 1, peca_detail.col)
+        let andada2 = this.getpecadetail(peca_detail.lin - 2, peca_detail.col)
+        if(!pecas.includes(andada1[0])){
+          posicpossiveis.push([peca_detail.lin- 1, peca_detail.col ])
+
+          if (peca_detail.piecemoves === 0 && !pecas.includes(andada2[0])) {
+            posicpossiveis.push([peca_detail.lin - 2, peca_detail.col])
+          }
         }
       } else {
-        posicpossiveis.push([peca_detail.col+ 1, peca_detail.lin ])
-        if (peca_detail.piecemoves === 0) {
-          posicpossiveis.push([peca_detail.col + 2, peca_detail.lin])
+        let diagonaldireita = this.getpecadetail(peca_detail.lin +1, peca_detail.col - 1)
+        let diagonalesquerda = this.getpecadetail(peca_detail.lin +1, peca_detail.col + 1)
+        if (pecas.includes(diagonaldireita[0]) && diagonaldireita[1] !== peca_detail.cor){
+          posicpossiveis.push([peca_detail.lin +1 , peca_detail.col - 1])
         }
+        if (pecas.includes(diagonalesquerda[0]) && diagonalesquerda[1] !== peca_detail.cor){
+          posicpossiveis.push([peca_detail.lin +1 , peca_detail.col + 1])
+        }
+        let andada1 = this.getpecadetail(peca_detail.lin + 1, peca_detail.col)
+        let andada2 = this.getpecadetail(peca_detail.lin + 2, peca_detail.col)
+        if(!pecas.includes(andada1[0])){
+          posicpossiveis.push([peca_detail.lin+ 1, peca_detail.col ])
+
+          if (peca_detail.piecemoves === 0 && !pecas.includes(andada2[0])) {
+            posicpossiveis.push([peca_detail.lin + 2, peca_detail.col])
+          }
+        }
+        
       }
     }
-    Array.from({ length: 8 }).map((_, lin) => (
-        Array.from({ length: 8 }).map((_, col) => {
-          const existe = posicpossiveis.some(p => p[0] === col && p[1] === lin);
+    Array.from({ length: 8 }).map((_, col) => (
+        Array.from({ length: 8 }).map((_, lin) => {
+          const existe = posicpossiveis.some(p => p[0] === lin && p[1] === col);
           let peca_detail =
-                  this.state.tabuleiro[col][lin] !== null
-                    ? this.state.tabuleiro[col][lin].split('_')
+                  this.state.tabuleiro[lin][col] !== null
+                    ? this.state.tabuleiro[lin][col].split('_')
                     : [null, null]
           if (existe) {
-            tabuleiro[col][lin] = peca_detail !== null? peca_detail[0]+'_'+ peca_detail[1] + '_o_' + peca_detail[3] : 'o_'
+            tabuleiro[lin][col] = peca_detail !== null? peca_detail[0]+'_'+ peca_detail[1] + '_o_' + peca_detail[3] : 'o_'
           }
             })
     ))
@@ -118,37 +154,37 @@ export default class Xadrez extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableHighlight
+        <TouchableOpacity
           style={styles.reiniciar}
           onPress={() => this.setState(initialstate)}
         >
           <Text style={{ color: 'white', fontWeight: '600' }}>Reiniciar</Text>
-        </TouchableHighlight>
+        </TouchableOpacity>
         <View style={styles.tabuleiro}>
-          {Array.from({ length: 8 }).map((_, lin) => (
-            <View key={lin} style={styles.lin}>
-              {Array.from({ length: 8 }).map((_, col) => {
-                const color = (lin + col) % 2 === 0 ? 'white' : 'black'
+          {Array.from({ length: 8 }).map((_, col) => (
+            <View key={col} style={styles.col}>
+              {Array.from({ length: 8 }).map((_, lin) => {
+                const color = (col + lin) % 2 === 0 ? 'white' : 'black'
                 let peca_detail =
-                  this.state.tabuleiro[col][lin] !== null
-                    ? this.state.tabuleiro[col][lin].split('_')
+                  this.state.tabuleiro[lin][col] !== null
+                    ? this.state.tabuleiro[lin][col].split('_')
                     : [null, null]
 
                 peca_detail = peca_detail[0] !== null && peca_detail[0] === 'o' ? [null,null,'o',null] : peca_detail
 
                 return (
                   <Quadrado
-                    key={`${lin}-${col}`}
-                    lin={lins[lin]}
-                    col={-1 * (col - 8)}
+                    key={`${col}-${lin}`}
+                    col={cols[col]}
+                    lin={-1 * (lin - 8)}
                     color={color}
                     size={this.quadradosize}
                     onPress={() =>
                       this.pecaclicada({
                         nome: peca_detail[0],
                         cor: peca_detail[1],
-                        lin: lin,
                         col: col,
+                        lin: lin,
                         selectable: peca_detail[2] === 'o',
                         piecemoves: peca_detail[3] ? parseInt(peca_detail[3]) : 0
 
@@ -158,8 +194,8 @@ export default class Xadrez extends React.Component {
                     <Peca
                       tipo={peca_detail[0]}
                       cor={peca_detail[1]}
-                      lin={lins[lin]}
-                      col={-1 * (col - 8)}
+                      col={cols[col]}
+                      lin={-1 * (lin - 8)}
                       select={peca_detail[2] === 'o'}
                     />
                   </Quadrado>
--- xadrez/src/components/Quadrado.js
+++ xadrez/src/components/Quadrado.js
@@ -4,8 +4,6 @@ import { TouchableHighlight, StyleSheet, View } from "react-native"
 export default  props => {
     const color = props.color === 'black'? '#739552' : '#EBECD0'
     const size = props.size
-    const col = props.col || 0
-    const lin = props.lin || 0
     const styles = StyleSheet.create({
         quadrado: {
             width: size || 50,
